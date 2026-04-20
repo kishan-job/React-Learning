808204275691033a6843c278a70bdd8bd218fc0f
kishan kancharla <109458667+kishan-job@users.noreply.github.com>
2026-04-20 13:02:00 +0000

    restructuring useContex

---
+++ ReactViteInterview/src/Practice/09useContext/02-model/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import HeaderContextChild from "./components/HeaderContextChild";
+import HeaderContextChild2 from "./components/HeaderContextChild2";
+function App() {
+  const num = 905;
+  const name = "Siii";
+  return (
+    <>
+        <HeaderContextChild />
+        <HeaderContextChild2 />
+     
+    </>
+  );
+}
+
+export default App;
